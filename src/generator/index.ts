ffd884035a17e07726716b33b02822ae458d5703
Roki Turner <roki@0xcaf2.dev>
2025-04-27 18:25:55 +0900

    feat: add generator for arithmetic

---
+++ src/generator/index.ts
@@ -0,0 +1,19 @@
+import * as Blockly from "blockly"
+
+class CalciumGenerator extends Blockly.Generator {
+  indent = 1
+
+  constructor() {
+    super("calcium")
+  }
+}
+
+export const calciumGenerator = new CalciumGenerator()
+
+export const removeParens = (codeStr: string) => {
+  let strWithoutParens = codeStr
+  if (codeStr && codeStr[0] === '(') {
+    strWithoutParens = codeStr.substring(1, codeStr.length - 1)
+  }
+  return strWithoutParens
+}
