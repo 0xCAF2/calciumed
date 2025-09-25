3e1d9d25380f159655967a7f5c370d379db26c85
Roki Turner <roki@0xcaf2.dev>
2025-09-25 09:23:06 +0900

    feat: add pseudo arithmetic and relational blocks

--- src/lang/ps-jp/tooltip.ts
+++ src/lang/ps-jp/tooltip.ts
@@ -1,17 +1,22 @@
 import { ConstantValues, tooltipManager } from "../../constant-manager"
 
+const PSEUDO_ARITHMETIC_TOOLTIP =
+  "数値や文字列に対して、四則演算や文字列の結合を行います。"
 const PSEUDO_ARRAY_TOOLTIP = "配列の要素を1つだけ指定します。"
 const PSEUDO_ARRAY_SLICE_TOOLTIP = "配列の範囲を決めて、要素を指定します。"
 const PSEUDO_ASSIGN_ARRAY_TOOLTIP = "新しい配列を代入します。"
-const PSEUDO_ASSIGN_ZERO_TOOLTIP = '配列の要素をすべて0に書き換えます。'
+const PSEUDO_ASSIGN_ZERO_TOOLTIP = "配列の要素をすべて0に書き換えます。"
 const PSEUDO_ASSIGNMENT_TOOLTIP = "変数や配列の中身を変更します。"
 const PSEUDO_INPUT_INT_TOOLTIP = "整数を入力から受け取って、変数に代入します。"
-const PSEUDO_INPUT_STR_TOOLTIP = "文字列を入力から受け取って、変数に代入します。"
+const PSEUDO_INPUT_STR_TOOLTIP =
+  "文字列を入力から受け取って、変数に代入します。"
 const PSEUDO_NUMBER_TOOLTIP = "数値を表します。"
+const PSEUDO_RELATIONAL_TOOLTIP = "2つの値を比較します。"
 const PSEUDO_STR_TOOLTIP = "文字列を表します。"
 const PSEUDO_VARIABLE_TOOLTIP = "変数の名前を表します。"
 
 const tooltips: ConstantValues = {
+  PSEUDO_ARITHMETIC_TOOLTIP,
   PSEUDO_ARRAY_TOOLTIP,
   PSEUDO_ARRAY_SLICE_TOOLTIP,
   PSEUDO_ASSIGN_ARRAY_TOOLTIP,
@@ -20,6 +25,7 @@ const tooltips: ConstantValues = {
   PSEUDO_INPUT_INT_TOOLTIP,
   PSEUDO_INPUT_STR_TOOLTIP,
   PSEUDO_NUMBER_TOOLTIP,
+  PSEUDO_RELATIONAL_TOOLTIP,
   PSEUDO_STR_TOOLTIP,
   PSEUDO_VARIABLE_TOOLTIP,
 }
