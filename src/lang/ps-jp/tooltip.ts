8a7d24c270ca18a38358ac027028adbd5b2f8b3c
Roki Turner <roki@0xcaf2.dev>
2025-09-26 09:30:21 +0900

    feat: add pseudo logical + not

--- src/lang/ps-jp/tooltip.ts
+++ src/lang/ps-jp/tooltip.ts
@@ -10,6 +10,8 @@ const PSEUDO_ASSIGNMENT_TOOLTIP = "変数や配列の中身を変更します。
 const PSEUDO_INPUT_INT_TOOLTIP = "整数を入力から受け取って、変数に代入します。"
 const PSEUDO_INPUT_STR_TOOLTIP =
   "文字列を入力から受け取って、変数に代入します。"
+const PSEUDO_LOGICAL_TOOLTIP = "2つの条件を論理演算します。"
+const PSEUDO_NOT_TOOLTIP = "条件を否定して、反転させます。"
 const PSEUDO_NUMBER_TOOLTIP = "数値を表します。"
 const PSEUDO_RELATIONAL_TOOLTIP = "2つの値を比較します。"
 const PSEUDO_STR_TOOLTIP = "文字列を表します。"
@@ -24,6 +26,8 @@ const tooltips: ConstantValues = {
   PSEUDO_ASSIGNMENT_TOOLTIP,
   PSEUDO_INPUT_INT_TOOLTIP,
   PSEUDO_INPUT_STR_TOOLTIP,
+  PSEUDO_LOGICAL_TOOLTIP,
+  PSEUDO_NOT_TOOLTIP,
   PSEUDO_NUMBER_TOOLTIP,
   PSEUDO_RELATIONAL_TOOLTIP,
   PSEUDO_STR_TOOLTIP,
