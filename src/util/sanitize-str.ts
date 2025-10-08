875e1543902884bc8964a2552b54a30e9534e4c0
Roki Turner <roki@0xcaf2.dev>
2025-10-09 07:32:06 +0900

    fix: rename variables

--- src/util/sanitize-str.ts
+++ src/util/sanitize-str.ts
@@ -1,25 +1,25 @@
-export function sanitizeStr(s: string): string {
+export function sanitizeStr(inputStr: string): string {
   // sanitize value: remove unescaped double quotes, keep escaped ones like \"
-  let out = ""
-  for (let i = 0; i < s.length; i++) {
-    const ch = s[i]
-    if (ch === '"') {
+  let result = ""
+  for (let i = 0; i < inputStr.length; i++) {
+    const char = inputStr[i]
+    if (char === '"') {
       // count preceding consecutive backslashes
       let j = i - 1
-      let bs = 0
-      while (j >= 0 && s[j] === "\\") {
-        bs++
+      let countOfBackslashes = 0
+      while (j >= 0 && inputStr[j] === "\\") {
+        countOfBackslashes++
         j--
       }
       // if odd number of backslashes, the quote is escaped -> keep it
-      if (bs % 2 === 1) {
-        out += '"'
+      if (countOfBackslashes % 2 === 1) {
+        result += '"'
       } else {
         // skip unescaped quote
       }
     } else {
-      out += ch
+      result += char
     }
   }
-  return out
+  return result
 }
